4a2572e674f95ee4041c9309e929bc68f9d6ad91
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-07 09:00:44 -0800

    Cleanup of banner, copyright, etc.

--- src/_config.js
+++ src/_config.js
@@ -4,6 +4,13 @@
 // This module set the document specifications `host`, `owner`, `project`, `branch`, and `doc`
 // as given by the URL, or if absent, as given by the current defaults
 //
+var banner = '<A HREF="https://github.com/slacgismo/docs-browser" TARGET="_blank">SLAC GISMo Docs Browser</A>';
+var year = (new Date()).getFullYear();
+if ( year > 2019 )
+{
+    year = "2019-" + year;
+}
+var copyright = '<A HREF="https://raw.githubusercontent.com/slacgismo/docs-browser/master/LICENSE" TARGET="_blank">Copyright (C) ' + year + ', Regents of the Leland Stanford Junior University</A>';
 var query = new URLSearchParams(window.location.search);
 var host = query.get('host');
 if ( host == null )
