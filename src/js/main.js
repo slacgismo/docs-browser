09c7200166311daf45f1dc79dd530ee28f578404
Jonathan Goncalves <jonathan.goncalves@gmail.com>
2019-11-08 14:29:21 -0800

    All defaults are set and information is getting queried

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
-var main = main || {}; // establish the main namespace
+// establish the main namespace - gets loaded with index.html
+var main = main || {}; 
 
 (function(ns) {
+    /**
+     * To execute when the document is ready.
+     * - Set defaults
+     * - Cache the DOM elements we need to operate on
+     * - Attach listeners
+     * - Request initial required data
+     */
     var onLoad = function() {
         // setup our defaults
         ns.defaultHost = 'github.com';
@@ -13,11 +21,30 @@ var main = main || {}; // establish the main namespace
         // get our dom elements
         ns.selectOptionEl = $('#branch-select');
         ns.refreshDataBtn = $('#refresh-data');
+        ns.hostInputEl = $('#host-input');
+        ns.userOrgInputEl = $('#user-org-input');
+        ns.projectInputEl = $('#project-input');
+        ns.listOfDocsEl = $('#list-of-docs');
 
+        ns.setDefaults();
         ns.setupListeners();
         ns.getBranchInformation();
+        ns.getContent();
     }
 
+    /**
+     * Set the defaults for the dom elements we care about
+     */
+    ns.setDefaults = function() {
+        ns.hostInputEl.val(ns.defaultHost);
+        ns.userOrgInputEl.val(ns.defaultOwner);
+        ns.projectInputEl.val(ns.defaultProject);
+    }
+
+    /**
+     * Request data to load as content from the given resource.
+     * We assume github style structure and params
+     */
     ns.getContent = function(host = ns.defaultHost, 
                              owner = ns.defaultOwner, 
                              project = ns.defaultProject, 
@@ -26,17 +53,30 @@ var main = main || {}; // establish the main namespace
         
         $.get(url).done(function(data) {
             console.warn('success', data)
-        }).fail(function(err) { 
+            let listItems = '';
+            data.forEach(function(item) {
+                listItems += '<a href="#" class="list-group-item list-group-item-action">' + item.name + '</a>'
+            });
+            ns.listOfDocsEl.html(listItems);
+        }).fail(function(err) {
+            // TODO: create error handler 
             console.warn('error', err)
         });
     }
 
+    /**
+     * Attach our required click and change listeners
+     */
     ns.setupListeners = function() {
         ns.refreshDataBtn.click(function() {
             alert('i was clicked')
         })
     }
 
+    /**
+     * Retrieve branch information for the given project and update
+     * the select/options with the data returned
+     */
     ns.getBranchInformation = function(host = ns.defaultHost, 
                                        owner = ns.defaultOwner, 
                                        project = ns.defaultProject) {
@@ -44,7 +84,7 @@ var main = main || {}; // establish the main namespace
 
         $.get(url).done(function(data) {
             console.warn('success', data)
-            
+            // build the options from the response
             var selectOptions = data.reduce(function(acc, currentVal, idx) {
                 var curOpt = '<option>' + currentVal.name + '</option>';
                 if(idx === 1) {
@@ -52,12 +92,14 @@ var main = main || {}; // establish the main namespace
                 }
                 return acc + curOpt;
             });
-
+            // attach the generated options to the dom
             ns.selectOptionEl.html(selectOptions);
-        }).fail(function(err) { 
+        }).fail(function(err) {
+            // TODO: create error handler
             console.warn('error', err)
         });
     }
 
+    // on document.ready, get everything going
     $(function() { onLoad() })
 })(main);
