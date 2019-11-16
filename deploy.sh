ea2d735bb6d9524ef0c23b3a7df40b3acc623872
David P. Chassin <dchassin@slac.stanford.edu>
2019-11-16 08:09:44 -0800

    Added local deployment support

--- deploy.sh
+++ deploy.sh
@@ -2,35 +2,42 @@
 
 function syntax()
 {
-    echo "Syntax: $(basename $0) buckets ..."
+    echo "Syntax: $(basename $0) src [localhost|buckets] ..."
 }
 
 if [ $# -lt 1 ]; then
     syntax > /dev/stderr
     exit 1;
-elif [ "$1" == "-h" -o "$1" == "--help" ]; then
+elif [ "$1" == "-h" -o "$1" == "--help" -o "$1" == "help" ]; then
     syntax
     exit 0
-elif [ ! -d "src" ]; then
-    echo "ERROR: $(basename $0) must be run from project root directory" > /dev/stderr
+elif [ ! -d "$1" ]; then
+    echo "ERROR: $(basename $0) folder '$1' not found" > /dev/stderr
     exit 2
 fi
 
-cd src
-if [ -z "$(ls)" ]; then
-    echo "ERROR: no files to deploy in src" > /dev/stderr
-    exit 3
-fi
-while [ $# -ge 1 ]; do
-    for file in $(ls) ; do
-        if [ -f "$file" ]; then
-            target="s3://$1/$file"
-            if [ -z "$(grep ^$file ../deploy.once)" -o -z "$(aws s3 ls $target)" ]; then
-                # echo $file
-                aws s3 cp "$file" "$target"
-                aws s3api put-object-acl --bucket "$1" --key "$file" --acl public-read 
-            fi
-        fi
-    done
+if [ "$2" == "localhost" ]; then
+    cd $1
+    python3 -m http.server 1>http-server.log 2>&1 &
+    open http://localhost:8000/index.html
+else
+    cd $1
+    if [ -z "$(ls)" ]; then
+        echo "ERROR: no files to deploy in $1" > /dev/stderr
+        exit 3
+    fi
     shift 1
-done
+    while [ $# -ge 1 ]; do
+        for file in $(ls) ; do
+            if [ -f "$file" ]; then
+                target="s3://$1/$file"
+                if [ -z "$(grep ^$file .deploy-once)" -o -z "$(aws s3 ls $target)" ]; then
+                    # echo $file
+                    aws s3 cp "$file" "$target"
+                    aws s3api put-object-acl --bucket "$1" --key "$file" --acl public-read 
+                fi
+            fi
+        done
+        shift 1
+    done
+fi
