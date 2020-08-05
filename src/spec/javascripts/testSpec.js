ba086544fe66b1b01df68c2ba8367880ddb5e6f9
Anna Peery <avpeery@gmail.com>
2020-08-05 08:37:49 -0700

    WIP: initial set up for ajax testing with jasmine. needs improvements

--- src/spec/javascripts/testSpec.js
+++ src/spec/javascripts/testSpec.js
@@ -7,35 +7,30 @@ describe("tests ajax", function() {
   afterEach(function() {
     jasmine.Ajax.uninstall();
   });
-
-  // it("return 'hello'", function () {
-  //   var result = ns.loadMarkdownContent();
-  //   expect(results.toBe("Hello"))
-  // });
-
-//   it ("Logs text from the service to the console", function () {
-
-//     var consoleSpy = spyOn(console, "log");
+  
+  it ("Logs text from the service to the console", function () {
+    var consoleSpy = spyOn(console, "log");
     
-//     jasmine.Ajax.stubRequest('/some/url').andReturn({
-//         "method": 'POST', 
-//         "contentType": 'text/plain',
-//         "data": data
-//       });
+    jasmine.Ajax.stubRequest('/some/url').andReturn({
+        "method": 'POST', 
+        "contentType": 'text/plain',
+        "data": data
+      });
     
-//     var result = ns.LoadMarkdownContent();
-//     expect(consoleSpy).toHaveBeenCalledWith('Hello from the world');
-//     expect(consoleSpy).toHaveBeenCalledWith('complete');
-//   });
-//   it("Prints error if the service fails", function () {
-//     var consoleSpy = spyOn(console, "log");
-//     jasmine.Ajax.stubRequest('/some/url').andReturn({
-//       "status": 500,
-//       "contentType": "text/plain"
-//     });
+    var result = ns.LoadMarkdownContent();
+    // expect(consoleSpy).toHaveBeenCalledWith();
+    expect(consoleSpy).toHaveBeenCalledWith('complete');
+  });
 
-//     var result = ns.loadMarkdownContent();
-//     expect(consoleSpy).toHaveBeenCalledWith("error");
-//     expect(consoleSpy).toHaveBeenCalledWith("complete");
-//   });
-// });
+  it("Prints error if the service fails", function () {
+    var consoleSpy = spyOn(console, "log");
+    jasmine.Ajax.stubRequest('/some/url').andReturn({
+      "status": 500,
+      "contentType": "text/plain"
+    });
+
+    var result = ns.loadMarkdownContent();
+    expect(consoleSpy).toHaveBeenCalledWith("error");
+    expect(consoleSpy).toHaveBeenCalledWith("complete");
+    });
+ });
